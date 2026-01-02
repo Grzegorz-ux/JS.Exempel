426890155302343345f72b214d5824f82356d483
Grzegorz-ux <grzegorz.glodedata@gmail.com>
2026-01-02 21:09:55 +0100

    Implement dynamic greeting based on time
    Add greeting logic based on the current hour.

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello!');
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+    greeting = 'Dobry Wieczor!';
+} else if (hourNow > 12) {
+   
+} else if (hourNow > 0) {
+    greeting = 'Dzien Dobry!';
+} else {
+    greeting = 'Welcome!';
+}
+
+document.write('<h3>' + greeting + '</h3>');
