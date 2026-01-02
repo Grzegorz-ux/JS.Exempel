305642719f279922279d7173988932e14a37c3b0
Grzegorz-ux <grzegorz.glodedata@gmail.com>
2026-01-02 22:23:12 +0100

    Fix greeting logic for time-based messages

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ var today = new Date();
 var hourNow = today.getHours();
 var greeting;
 
-if (hourNow > 18) {
-    greeting = 'Dobry Wieczor!';
-} else if (hourNow > 12) {
-   
-} else if (hourNow > 0) {
-    greeting = 'Dzien Dobry!';
+if (hourNow >= 18) {
+    greeting = 'Dobry wieczór!';
+} else if (hourNow >= 12) {
+    greeting = 'Dzień dobry!';
+} else if (hourNow >= 0) {
+    greeting = 'Dzień dobry!';
 } else {
     greeting = 'Welcome!';
 }
